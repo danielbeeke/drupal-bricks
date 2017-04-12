efa83081db5696cc083ae45d4255e1462d8f8d81
Daniel Beeke <daniel@studiofonkel.nl>
2017-04-12 22:50:57 +0200

    Improving the javascript validation

--- js/nesting-validation.js
+++ js/nesting-validation.js
@@ -15,13 +15,33 @@
 
                 var _validIndentInterval = Drupal.tableDrag.prototype.row.prototype.validIndentInterval;
 
+                var getFirstPrevWithDifferentDepth = function (element) {
+                    var found = false;
+                    var initialDepth = $(element).prev('tr').attr('data-depth');
+                    while ($(element).length && !found) {
+                        element = $(element).prev('tr')[0];
+
+                        if ($(element).attr('data-depth') != initialDepth) {
+                            found = true;
+                        }
+                    }
+
+                    return element;
+                };
+
                 Drupal.tableDrag.prototype.row.prototype.validIndentInterval = function (prevRow, nextRow) {
                     var values = _validIndentInterval.apply(this, arguments);
                     var currentBundle = $(this.element).attr('data-bundle');
                     var prevBundle = $(prevRow).attr('data-bundle');
-                    var parentBundle = $(this.group[0]).prev('tr').attr('data-bundle');
+                    var parent = getFirstPrevWithDifferentDepth(this.element);
+                    var parentBundle = $(parent).attr('data-bundle');
 
-                    if ($.inArray(prevBundle, settings.bricks.nesting[currentBundle]) !== -1 || $.inArray(parentBundle, settings.bricks.nesting[currentBundle]) !== -1) {
+
+                    // console.log(parent)
+
+                    if ($.inArray(prevBundle, settings.bricks.nesting[currentBundle]) !== -1 ||
+                        $.inArray(parentBundle, settings.bricks.nesting[currentBundle]) !== -1
+                    ) {
                         values.max = 1;
                     }
                     else {
